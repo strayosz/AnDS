d6ee896e14f5645b9df86b1696d93136a1ce29f5
strayosz <kamilyakupov01@gmail.com>
2025-04-27 02:19:26 +0300

    Result

--- Java/src/main/java/Main.java
+++ Java/src/main/java/Main.java
@@ -2,6 +2,64 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("sadf");
+         String[] validCases = {
+                "",     // пустая строка
+                "()",   // простые валидные
+                "{}",
+                "[]",
+                "({[]})",  // вложенные валидные
+                "[{()}]",
+                "({})[]",
+                "(){}[]",  // сложные валидные
+                "({[]})[{}]",
+                "{[()]}"
+        };
+
+        String[] invalidCases = {
+                "(",   // незакрытые
+                ")",
+                "[",
+                "]",
+                "{",
+                "}",
+                "(]",   // несоответствующие типы
+                "{)",
+                "[}",
+                "{(})",  // неправильная вложенность
+                "({[}])",
+                "([)]",
+                "{[}]",
+                ")(",   // неправильный порядок
+                "][",
+                "}{",
+                "()[]{)",  // смешанные невалидные
+                "({[}])",
+                "{([]})"
+        };
+        for (String s: validCases) {
+            System.out.println(check(s)); //Все тесты должны быть true
+        }
+        System.out.println();
+        for (String s: invalidCases) {
+            System.out.println(check(s)); //Все тесты должны быть false
+        }
+    }
+
+    public static boolean check(String s) {
+        Stack stack = new Stack();
+        for(int pos = 0; pos < s.length(); pos++) {
+            if (s.charAt(pos) != ']' && s.charAt(pos) != '}' && s.charAt(pos) != ')') {
+                stack.add(s.charAt(pos));
+            } else {
+                if (stack.take() == null
+                        || (s.charAt(pos) == '}' && stack.take().getValue() != '{')
+                        || (s.charAt(pos) == ')' && stack.take().getValue() != '(')
+                        || (s.charAt(pos) == ']' && stack.take().getValue() != '[')) {
+                    return false;
+                }
+                stack.delete();
+            }
+        }
+        return stack.take() == null;
     }
 }
--- Java/src/main/java/Stack.java
+++ Java/src/main/java/Stack.java
@@ -1,2 +1,30 @@
 public class Stack {
+    private char value;
+    private Stack prev;
+    private Stack last;
+
+    public Stack(){}
+
+    public Stack(char value) {
+        this.value = value;
+        this.last = this;
+    }
+
+    public void add(char value) {
+        Stack temp = new Stack(value);
+        temp.prev = last;
+        this.last = temp;
+    }
+
+    public Stack take() {
+        return last;
+    }
+
+    public void delete() {
+        last = last.prev;
+    }
+
+    public char getValue() {
+        return value;
+    }
 }
