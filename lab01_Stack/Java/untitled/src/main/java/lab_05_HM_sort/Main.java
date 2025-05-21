85171709b126b4168e73258f49750ba41243dc06
strayosz <kamilyakupov01@gmail.com>
2025-05-21 13:09:44 +0300

    edit

---
+++ lab01_Stack/Java/untitled/src/main/java/lab_05_HM_sort/Main.java
@@ -0,0 +1,75 @@
+package lab_05_HM_sort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        task1(new int[] {90, 89, 88, 87, 80, 79, 78, 77, 76, 75, 71, 72, 73, 74, 75, 76});
+        task2("bly is the sky");
+        task3(new int[] {3, 30, 34, 5, 9});
+        task4(new int[] {15, 5, 11, 10, 12, 5, 5}, 30);
+    }
+
+    public static void task1(int[] x) {
+        int l = 0;
+        int r = x.length - 1;
+        int m;
+        while (l <= r) {
+            m = (l + r)/2;
+            if (x[m] > x[m + 1]) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        System.out.println(l - 1);
+    }
+
+    public static void task2(String text) {
+        int index = 0;
+        String result = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == ' ') {
+                result = text.substring(index, i) + " " + result;
+                index = i + 1;
+            }
+        }
+        result = text.substring(index, text.length()) + " " + result;
+        System.out.println(result);
+    }
+
+    public static void task3(int[] x) {
+        /**
+         * Отрывок из метода merge в MergeSort, если predication = true
+         * int Ili = l[i];
+         * String li = Integer.toString(Ili);
+         * int Iri = r[i];
+         * String ri = Integer.toString(Iri);
+         * //l[i] >= r[j]
+         * if (Integer.parseInt(li + ri) >= Integer.parseInt(ri + li)) {
+         *     a[k++] = l[i++];
+         * }
+         * else {
+         *     a[k++] = r[j++];
+         * }
+         */
+        Sort.mergeSort(x, x.length, true);
+        System.out.println(Arrays.toString(x));
+    }
+
+    public static void task4(int[] x, int s) {
+        Sort.mergeSort(x, x.length, false);
+        int sum;
+        for (int i = 0; i < x.length; i++) {
+            sum = 0;
+            for (int j = 0; j < i; j++) {
+                sum += x[j];
+            }
+            if (sum > s) {
+                System.out.println(i - 1);
+                break;
+            }
+
+        }
+    }
+}
---
+++ lab01_Stack/Java/untitled/src/main/java/lab_05_HM_sort/Sort.java
@@ -0,0 +1,57 @@
+package lab_05_HM_sort;
+
+public class Sort {
+    public static void mergeSort(int[] a, int n, boolean prediction) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid, prediction);
+        mergeSort(r, n - mid, prediction);
+
+        merge(a, l, r, mid, n - mid, prediction);
+    }
+
+    public static void merge(
+            int[] a, int[] l, int[] r, int left, int right, boolean prediction) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (!prediction) {
+                if (l[i] <= r[j]) {
+                    a[k++] = l[i++];
+                }
+                else {
+                    a[k++] = r[j++];
+                }
+            } else {
+                int Ili = l[i];
+                String li = Integer.toString(Ili);
+                int Iri = r[i];
+                String ri = Integer.toString(Iri);
+                //l[i] >= r[j]
+                if (Integer.parseInt(li + ri) >= Integer.parseInt(ri + li)) {
+                    a[k++] = l[i++];
+                }
+                else {
+                    a[k++] = r[j++];
+                }
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+}
